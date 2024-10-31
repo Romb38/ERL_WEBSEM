c4b58f590124036e3fe5b5ae3722d3ca1c7276b3
Sube <lucas.sube@etu.univ-grenoble-alpes.fr>
2024-10-31 11:15:32 +0100

    add a function to fetch songs from an artist

--- DeezerAPI.py
+++ DeezerAPI.py
@@ -44,69 +44,6 @@ def getTitleFromArtistID(artist_id):
         return f"Une erreur est survenue : {str(e)}"
 
 
-"""
-Exemple pour id 13
-
-{
-    'data': [
-        {
-            'album': {
-                'cover': 'https://api.deezer.com/album/119606/image',
-                'cover_big': 'https://e-cdns-images.dzcdn.net/images/cover/e2b36a9fda865cb2e9ed1476b6291a7d/500x500-000000-80-0-0.jpg',
-                'cover_medium': 'https://e-cdns-images.dzcdn.net/images/cover/e2b36a9fda865cb2e9ed1476b6291a7d/250x250-000000-80-0-0.jpg',
-                'cover_small': 'https://e-cdns-images.dzcdn.net/images/cover/e2b36a9fda865cb2e9ed1476b6291a7d/56x56-000000-80-0-0.jpg',
-                'cover_xl': 'https://e-cdns-images.dzcdn.net/images/cover/e2b36a9fda865cb2e9ed1476b6291a7d/1000x1000-000000-80-0-0.jpg',
-                'id': 119606,
-                'md5_image': 'e2b36a9fda865cb2e9ed1476b6291a7d',
-                'title': 'Curtain Call: The Hits',
-                'tracklist': 'https://api.deezer.com/album/119606/tracks',
-                'type': 'album'
-            },
-            'artist': {
-                'id': 13,
-                'name': 'Eminem',
-                'tracklist': 'https://api.deezer.com/artist/13/top?limit=50',
-                'type': 'artist'
-            },
-            'contributors': [
-                {
-                    'id': 13,
-                    'link': 'https://www.deezer.com/artist/13',
-                    'name': 'Eminem',
-                    'picture': 'https://api.deezer.com/artist/13/image',
-                    'picture_big': 'https://e-cdns-images.dzcdn.net/images/artist/19cc38f9d69b352f718782e7a22f9c32/500x500-000000-80-0-0.jpg',
-                    'picture_medium': 'https://e-cdns-images.dzcdn.net/images/artist/19cc38f9d69b352f718782e7a22f9c32/250x250-000000-80-0-0.jpg',
-                    'picture_small': 'https://e-cdns-images.dzcdn.net/images/artist/19cc38f9d69b352f718782e7a22f9c32/56x56-000000-80-0-0.jpg',
-                    'picture_xl': 'https://e-cdns-images.dzcdn.net/images/artist/19cc38f9d69b352f718782e7a22f9c32/1000x1000-000000-80-0-0.jpg',
-                    'radio': True,
-                    'role': 'Main',
-                    'share': 'https://www.deezer.com/artist/13?utm_source=deezer&utm_content=artist-13&utm_term=0_1728744490&utm_medium=web',
-                    'tracklist': 'https://api.deezer.com/artist/13/top?limit=50',
-                    'type': 'artist'
-                }
-            ],
-            'duration': 326,
-            'explicit_content_cover': 0,
-            'explicit_content_lyrics': 1,
-            'explicit_lyrics': True,
-            'id': 1109731,
-            'link': 'https://www.deezer.com/track/1109731',
-            'md5_image': 'e2b36a9fda865cb2e9ed1476b6291a7d',
-            'preview': 'https://cdnt-preview.dzcdn.net/api/1/1/2/7/a/0/27a14827ff1e82c5e40e8b6a934a8637.mp3?hdnea=exp=1728745390~acl=/api/1/1/2/7/a/0...c5e40e8b6a934a8637.mp3*~data=user_id=0,application_id=42~hmac=f109783a93ede2d4ee7ae884e0c21b83e7b0d18f8510a19d27a3d74c9fece43a',
-            'rank': 982936,
-            'readable': True,
-            'title': 'Lose Yourself',
-            'title_short': 'Lose Yourself',
-            'title_version': '',
-            'type': 'track'
-        }
-    ],
-    'next': 'https://api.deezer.com/artist/13/top?limit=1&index=1',
-    'total': 100
-}
-
-
-"""
 
 
 
@@ -159,3 +96,54 @@ def getTitleFromIncreasingArtistId(nb_title, base_artist_id=1):
             out.append(title)
         sleep(0.2)
     return out
+
+
+
+
+def getDataFromArtistID(artist_id):
+    """
+    Get from Deezer API the data of the artist
+    :param artist_id: Integer - ID of the artist
+    :return: JSON data which represent the title of the artist or None if it doesn't exist
+    """
+    url = f"{BASE_URL}/artist/{artist_id}/top?limit=1"
+    try:
+        # Effectue la requête GET
+        response = requests.get(url)
+        # Vérifie que la requête a réussi
+        if response.status_code == 200:
+            # Parse la réponse en JSON
+            data = response.json()
+            # Vérifie s'il y a des données
+            if 'data' in data and len(data['data']) > 0:
+                return data
+            else:
+                return None
+        else:
+            return f"Erreur {response.status_code} lors de la requête."
+
+    except Exception as e:
+        return f"Une erreur est survenue : {str(e)}"
+
+
+def getAllTracksFromData(data):
+    """
+    Get an array of tracks name from data fetched from Deeze API
+    :param data: Dictionnary - data of the artist
+    :return: array of tracks name from the artist
+    """
+    album_id = data['data'][0]['album']['id']
+    response = requests.get(f"{BASE_URL}/album/{album_id}/tracks")
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and len(data['data']) > 0:
+            # Extrait les titres des pistes
+            tracks = [track['title'] for track in data['data']]
+            return tracks
+        else:
+            return None
+    else:
+        return f"Une erreur est survenue : {str(e)}"
+
+    return tracks
+
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import DeezerAPI as DAPI
 
 def main():
     DAPI.getTitleFromArtistID(13)
+    data = DAPI.getDataFromArtistID(13)
+    tracks = DAPI.getAllTracksFromData(data)
+    print(tracks)
     return
 
 
