c850e74e9195d8e813dadfdff57f71a2c9789c29
m2gi-bernete <elsa.bernet38@gmail.com>
2024-11-19 17:14:18 +0100

    Récupération legal name des artistes suivant convention

--- MusicBrainzAPI.py
+++ MusicBrainzAPI.py
@@ -1,4 +1,5 @@
 # Import the module
+from time import sleep
 import musicbrainzngs
 
 def set_usagent():
@@ -9,6 +10,8 @@ def set_usagent():
 
 def getDataFromArtistID(artist_id):
     try:
+        sleep(0.2)
+        #Récupérer les artistes depuis leurs id en incluant leurs alias
         result = musicbrainzngs.get_artist_by_id(artist_id, includes=["aliases"])
     #TODO : traiter exceptions correctement
     except Exception as exc:
@@ -20,24 +23,61 @@ def getDataFromArtistID(artist_id):
         return artist
 
 def searchUSArtistByName(artist_name):
-    #Strict true car sinon req sans fields assignés
-    result = musicbrainzngs.search_artists(strict=True, artist="Eminem", country="US")
-
+    sleep(0.2)
+    #Récupérer les artistes avec leur nom, qui sont aux US
+    #Strict true car sinon req ne suite pas la demande
+    result = musicbrainzngs.search_artists(strict=True, artist=artist_name, country="US")
+    
     for artist in result['artist-list']:
         print(u"{id}: {name}".format(id=artist['id'], name=artist['name']))
+    
 
-def getArtistLegalName(artist_id):
+def searchArtistByName(artist_name):
+    sleep(0.2)
+    #Récupérer le 1er artiste depuis son nom incluant ses alias
+    #Strict true car sinon req ne suite pas la demande
+    result = musicbrainzngs.search_artists(strict=True, limit=1, artist=artist_name, **{'alias':''})
+    
+    #Liste donc retourner l'élement 0
+    return result['artist-list'][0]
+
+    #for artist in result['artist-list']:
+    #    print(u"{id}: {name}".format(id=artist['id'], name=artist['name']))
+
+def getArtistLegalNameById(artist_id):
     try:
+        sleep(0.2)
         result = musicbrainzngs.get_artist_by_id(artist_id, includes=['aliases'])
 
     except Exception as exc:
         print("Something went wrong with the request: %s" % exc)
     else:
         artist = result["artist"]
+        if (artist['type'] == 'Person'):
+            for alias in artist['alias-list']:
+                #if alias['type'] == 'Artist name' and ('locale' in alias and alias['locale'] == 'en'):
+                #    print("Artist name (en):\t\t%s" % alias["alias"])
+                if alias['type'] == 'Legal name':
+                    legalName = alias["alias"]
+                    print("Legal name:\t\t%s" % alias["alias"])
+            return legalName
+    
+    
+def getArtistLegalName(artist_name):
+    artist = searchArtistByName(artist_name)
+    locale_list = ['fr', 'fr_FR', 'en', 'en_US']
+
+    #Si groupe il n'y pas pas de nom légal
+    if (artist['type'] == 'Person'):
         for alias in artist['alias-list']:
-            print(alias)
-            if alias['type'] == 'Artist name' and alias['locale'] == 'en':
-                print("Artist name (en):\t\t%s" % alias["alias"])
-            elif alias['type'] == 'Legal name':
-                print("Legal name:\t\t%s" % alias["alias"])
-        return result
+            if 'type' in alias and alias['type'] == 'Legal name' :
+                
+                #Cas où locale existe en plusieurs langues : garder seulement langues précisées
+                #Cas où locale n'existe pas
+                #renvoyer
+                if ('locale' in alias and (alias['locale'] in locale_list)) or ('locale' not in alias):
+                    legalName = [x.strip() for x in alias['sort-name'].split(',')]
+
+        return legalName
+    
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import DeezerAPI as DAPI
 import MusicBrainzAPI as MBAPI
 
 def main():
+    #Identification pour accéder à l'API MusicBrainz
     MBAPI.set_usagent()
 
     DAPI.getTitleFromArtistID(13)
@@ -9,9 +10,11 @@ def main():
     tracks = DAPI.getAllTracksFromData(data)
     print(tracks)
     print("\n")
-    MBAPI.getDataFromArtistID("db92a151-1ac2-438b-bc43-b82e149ddd50")
-    MBAPI.searchUSArtistByName("Eminem")
-    MBAPI.getArtistLegalName("b95ce3ff-3d05-4e87-9e01-c97b66af13d4")
+    
+    #MBAPI.getDataFromArtistID("db92a151-1ac2-438b-bc43-b82e149ddd50")
+    #MBAPI.searchArtistByName("Eminem")
+    print(MBAPI.getArtistLegalName("rick astley"))
+    #MBAPI.getArtistLegalNameById("b95ce3ff-3d05-4e87-9e01-c97b66af13d4")
     return
 
 
