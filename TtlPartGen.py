edc57183dd58de6ec2c5f3baaf49616c369c00d9
barbiero <r.barbier38@gmail.com>
2024-11-20 14:35:47 +0100

    Refactoring de la génération de turtle

--- TtlPartGen.py
+++ TtlPartGen.py
@@ -1,5 +1,37 @@
 import tools
 
+
+def genIDS(
+        ARTIST_NAMES,
+        SONG_IDS,
+        ALBUM_IDS,
+        KINDS
+):
+    """
+    Génère la totalité des IDs interne des différentes données
+
+    @param ARTIST_IDS Liste des noms de tous les artistes
+    @param SONG_IDS Liste des IDs de Deezer de toutes les chansons (tout artistes confondus)
+    @param ALBUM_IDS Liste des IDs de Deezer de tous les albums (tout artistes confondu)
+    @param KIND Liste de tous les nom de genre de Deezer (tout artistes confondu)
+    
+    """
+    for ARTIST_NAME in ARTIST_NAMES :
+        ID_Artist_Name = tools.formatLine(ARTIST_NAME)
+        tools.ARTIST_LINK[ARTIST_NAME] = ID_Artist_Name
+    
+    for SONG_ID in SONG_IDS:
+        UUID_SONG = tools.getUUID()
+        tools.SONG_LINK[SONG_ID] = UUID_SONG
+    
+    for ALBUM_ID in ALBUM_IDS :
+        UUID_ALBUM = tools.getUUID()
+        tools.ALBUM_LINK[ALBUM_ID] = UUID_ALBUM
+    
+    for KIND in KINDS :
+        KIND_LABEL = tools.formatLine(KIND)
+        tools.KIND_LINK[KIND] = KIND_LABEL 
+
 def genPrefix() :
     return """
     @prefix foaf: <http://xmlns.com/foaf/0.1/> .
@@ -12,53 +44,82 @@ def genPrefix() :
     """
 
 def genArtist(
+        ARTIST_NAME,
         artistName,
         givenName,
         familyName,
-        UUID_ALBUMS,
-        UUID_SONGS,
+        ALBUM_IDS,
+        SONG_IDS,
 ):
-    ID_Artist_Name = tools.formatLine(artistName)
-    ALBUMS = "\n".join(f"erlo:produce erl:{UUID_ALBUM} ;" for UUID_ALBUM in UUID_ALBUMS)
-    SONG = "\n".join(f"erlo:compose erl:{UUID_SONG} ;" for UUID_SONG in UUID_SONGS)
+    ID_Artist_Name = tools.ARTIST_LINK[ARTIST_NAME]
+    ALBUMS = "\n".join(f"erlo:produce erl:{tools.ALBUM_LINK[ALBUM_ID]} ;" for ALBUM_ID in ALBUM_IDS)
+    SONG = "\n".join(f"erlo:compose erl:{tools.SONG_LINK[SONG_ID]} ;" for SONG_ID in SONG_IDS)
+    
+    if givenName : 
+        GIVEN_NAME = "foaf:givenName \"{givenName}\" ;\n"
+    else :
+        GIVEN_NAME = ""
+
+    if familyName : 
+        FAMILY_NAME = "foaf:familyName \"{familyName}\" ;\n"
+    else :
+        FAMILY_NAME = ""
 
     return (ID_Artist_Name, f"""
     erl:{ID_Artist_Name} a foaf:Person
         dbo:artist dbr:{ID_Artist_Name} ;
-        foaf:givenName "{givenName}" ;
-        foaf:familyName "{familyName}" ;
+        {GIVEN_NAME}
+        {FAMILY_NAME}
         dbo:alias "{artistName}" ;
         owl:sameAs dbr:{ID_Artist_Name} ;
         {ALBUMS}
-        {SONG}
+        {SONG[:-2]} .
         .
     """)
 
 def genSong(
+    SONG_ID,
     ID_ARTIST_NAME,
     SONG_NAME,
-    IDS_FEATURED_ARTIST
+    FEATURED_ARTIST_NAMES
 ):
-    UUID_SONG = tools.getUUID()
-    FEATURED_ARTIST = "\n".join(f"dbo:featuredArtist erl:{ID_FEATURED_ARTIST} ;" for ID_FEATURED_ARTIST in IDS_FEATURED_ARTIST)
+    UUID_SONG = tools.SONG_LINK[SONG_ID]
+    if (len(FEATURED_ARTIST_NAMES) > 0) :
+        FEATURED_ARTIST = "\n".join(f"dbo:featuredArtist erl:{tools.ARTIST_LINK[FEATURED_ARTIST_NAME]} ;" for FEATURED_ARTIST_NAME in FEATURED_ARTIST_NAMES)
+    else : 
+        FEATURED_ARTIST = ""
+
 
     return (UUID_SONG, f"""
     erl:{UUID_SONG} a dbo:Song
         foaf:Person erl:{ID_ARTIST_NAME} ;
         rdfs:label "{SONG_NAME}" ;
-        dbo:Album erl:IDAlbum ;
-        {FEATURED_ARTIST}
+        {FEATURED_ARTIST[:-2]} .
     .
     """)
 
-def genAlbum():
-    pass
+def genAlbum(
+        ALBUM_ID,
+        ALBUM_NAME,
+        ARTIST_NAME,
+        KIND_LABEL,
+        SONGS_IDS
+):
+    UUID_ALBUM = tools.ALBUM_LINK[ALBUM_ID]
+    ID_ARTIST_NAME = tools.ARTIST_LINK[ARTIST_NAME]
+    SONGS = "\n".join(f"erlo:contains erl:{tools.SONG_LINK[SONG_ID]} ;" for SONG_ID in SONGS_IDS)
+    return(UUID_ALBUM, f"""
+    erl:{UUID_ALBUM} a dbo:Album
+        rdfs:label "{ALBUM_NAME}" ;
+        foaf:Person erl:{ID_ARTIST_NAME} ;
+        dbo:genre erl:{KIND_LABEL} ;
+        {SONGS[:-2]} .
+    """)
 
 def genKind(
     KIND,
 ):
-    KIND_LABEL = tools.formatLine(KIND)
-
+    KIND_LABEL = tools.KIND_LINK[KIND]
     return (KIND_LABEL, f"""
     erl:{KIND_LABEL} a dbo:genre
         rdfs:label "{KIND}" ;
--- tools.py
+++ tools.py
@@ -1,5 +1,10 @@
 import shortuuid
 
+ARTIST_LINK = {}
+ALBUM_LINK = {}
+SONG_LINK = {}
+KIND_LINK = {}
+
 def getUUID():
     su = shortuuid.ShortUUID(alphabet="1234567890")
     return su.random(length=8)
